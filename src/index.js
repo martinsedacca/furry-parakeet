c825e9a926e0a02d95c3330ed281a03a7b56045f
[Martin] <[m.sedacca@gmail.com]>
2020-05-02 03:49:39 -0500

    vue

--- src/index.js
+++ src/index.js
@@ -1,7 +1,29 @@
-import { saludar } from './js/componentes.js';
-import './styles.css';
 
 
-const nombre = 'Fernando';
+const app = new Vue({
+    el: '#app',
+    data: {
+        titulo: 'Hola mundo con Vue',
+        nuevaFruta: '',
+        frutas: [{
+            nombre: 'Platano',
+            cantidad: 1
+        },
+        {
+            nombre: 'Manzana',
+            cantidad: 10
+        },
+        {
+            nombre: 'Kiwi',
+            cantidad: 0
+        }
+    ]
+    
+    },
+   methods:{
+    agregarFruta(){
+        console.log('diste click');
+    }
+   }
+});
 
-saludar( nombre );
